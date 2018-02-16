90c8f4d6512b5991fa0c8475ba2f741ea05d407c
Emiliano <emiliano@Emilianos-MacBook-Pro.local>
2018-02-16 18:07:58 +0100

    mailer

--- mailer.php
+++ mailer.php
@@ -1,8 +1,45 @@
 <?php
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
 $name=$_POST["name"];
 $cog=$_POST["cog"];
-$mail=$_POST["mail"];
+$sender=$_POST["mail"];
 $msg=$_POST["msg"];
+
+
+$mail = new PHPMailer(true);  
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'user@example.com';                 // SMTP username
+    $mail->Password = 'secret';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('ellen@example.com');
+    $mail->addCC('cc@example.com');
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
+
+
+
 
 $subject='Nuovo messaggio da '.$name.' '.$cog.' - '.$mail;
 $to = 'emil.costanzo@gmail.com';
